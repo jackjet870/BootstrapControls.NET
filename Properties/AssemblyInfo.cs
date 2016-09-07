47bcccd4935fe71fc5822600e05e9053ecf7b9bd
Paweł Niewiadomski <pawel.niewiadomski@loyaltypoint.pl>
2016-09-07 13:06:20 +0200

    cleanup

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BootstrapControls")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("BootstrapControls.NET")]
+[assembly: AssemblyDescription("A set of useful webforms controls for bootstrap")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Maple Paweł Niewiadomski")]
-[assembly: AssemblyProduct("BootstrapControls")]
-[assembly: AssemblyCopyright("Copyright © Paweł Niewiadomski 2015 - http://maple.com.pl")]
+[assembly: AssemblyCompany("Loyalty Point")]
+[assembly: AssemblyProduct("BootstrapControls.NET")]
+[assembly: AssemblyCopyright("Copyright © Paweł Niewiadomski 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
